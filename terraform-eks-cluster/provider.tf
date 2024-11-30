010a6249b875c680df31dde9cae1a7f1e5ce7e17
Anil Patel <anilkupatel001@gmail.com>
2024-11-30 18:26:11 +0000

    updating kubernetes auth

--- terraform-eks-cluster/provider.tf
+++ terraform-eks-cluster/provider.tf
@@ -39,4 +39,5 @@ provider "kubernetes" {
   host                   = data.aws_eks_cluster.main.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.main.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.main.token
+  load_config_file       = false
 }
